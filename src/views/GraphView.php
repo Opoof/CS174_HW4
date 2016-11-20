d2900c513c55e0b90c80ce28cea337c935370860
Ryan Daily <ryan.daily.et@gmail.com>
2016-11-20 02:51:06 -0800

    Cleaned up merge conflict wtih Ryan Branch

--- src/views/GraphView.php
+++ src/views/GraphView.php
@@ -6,6 +6,17 @@ require_once("View.php");
 
 class GraphView implements View{
 	function render($data){
-		
+		?>
+			<!DOCTYPE html>
+			<html>
+				<head>
+					<link rel="stylesheet" type="text/css" href="src/styles/style.css">
+				</head>
+				<body>
+					<h1 class="header">hello there</h1>
+					<p><?php print_r($data); ?></p>	
+				</body>
+			</html>
+		<?php
 	}
 }
