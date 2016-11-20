4d96b3f7d74f1980d4223ffc78169589ad9febc1
Ryan Daily <ryan.daily.et@gmail.com>
2016-11-19 18:52:50 -0800

    Undid dark mode

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once("src/controllers/LandingController.php");
 require_once("src/controllers/GraphController.php");
 
+
 if(isset($_REQUEST["submit"])){
 	$controller = new GraphController();
 }
